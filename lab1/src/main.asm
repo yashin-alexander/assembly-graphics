cc63984c6e3e9b0d6216d6acaba1b04d0f1bdbb8
yashin-alexander <alexandr.yashin@emlid.com>
2018-09-17 00:23:18 +0300

    lab1/src/main.asm: create bresenham algorithm procedure sample
    - wrong namings fixed
    - created checks for incorrect values in `transform coordinate` procedure

--- lab1/src/main.asm
+++ lab1/src/main.asm
@@ -1,46 +1,96 @@
 ; =============================================================================
 
 .model small
+.data
+    a_x db 0
+    a_y db 0
+    b_x db 0
+    b_y db 0
+    bresenham_delta db 0
 .code
 
 
 main proc near
-    call setup_sga_videobuffer
+    call setup_cga_videomode
 
-    mov bh, 99
-    mov bl, 14
-    call transform_coordinates
-    call draw_white_point
-    ; mov ax, 0000
-    ; mov bx, 1414
-    ; call draw_line
+    ; xor bx, bx
+    ; mov bx, 0069h
+    ; call transform_coordinates
+    ; call draw_white_point
+
+    mov ax, 0000h
+    mov bx, 3030h
+    call draw_line
     call wait_for_keypress
     call exit
     main endp
 
 
-setup_sga_videobuffer proc near
-                        ; set up es to be a pointer on videobuffer address
+setup_cga_videomode proc near
+; set up es to be a pointer on videobuffer address
     mov ax, 0006h ; 6 - white & black cga mode
     int 10h
     mov ax, 0b800h ; 0b80:0000 - first byte of cga videobuffer
     mov es, ax
     ret
-    setup_sga_videobuffer endp
+    setup_cga_videomode endp
+
 
 draw_line proc near
-                        ; ah:al - y:x coordinates of point A
-                        ; bh:bl - y:x coordinates of point B
-    
+; ah:al - y:x coordinates of point A
+; bh:bl - y:x coordinates of point B
+    mov a_x, al
+    mov a_y, ah
+    mov b_x, bl
+    mov b_y, bh
+    call calculate_deltas 
+
+    mov cl, b_x
+    sub cl, a_x          ; calculate (x1-x0), put in cl
+
+    mov dl, a_x          ; dl contains current x-position
+    mov dh, a_y          ; dh containt current y-position
+    bresenham_algorithm:
+        mov bl, dl
+        mov bh, dh
+
+        call transform_coordinates
+        call draw_white_point
+
+        inc dl           ; increment x coordinate
+        add dh, bresenham_delta
+        loop bresenham_algorithm
     ret
     draw_line endp
 
 
+calculate_deltas proc near
+; ah:al - y0:x0
+; bh:bl - y1:x1
+; calculate (y1 - y0)/(x1 - x0) 
+    mov dl, bl
+    sub dl, al ; x1 - x0
+    mov dh, bh
+    sub dh, ah ; y1 - y0
+
+    xor ax, ax
+    mov al, dh ; y1 - y0 -> al
+    div dl     ; al = (x1 - x0) / (y1 - y0)
+    mov bresenham_delta, al
+
+    ret
+    calculate_deltas endp
+
+
 transform_coordinates proc near
-                        ; bh - y coordinate [0-99]
-                        ; bl - x coordinate [0-79]
-                        ; returns numeric representation of address in bx
-                        ; affects ax
+; bh - y coordinate [0-99]
+; bl - x coordinate [0-79]
+; exit with error if input values are out of bounds
+; returns numeric representation of address in bx
+    cmp bh, 100
+    jae exit
+    cmp bl, 80
+    jae exit
     mov al, 80 ; 80 pixels in line
     mul bh
     xor bh, bh
@@ -50,8 +100,8 @@ transform_coordinates proc near
 
 
 draw_white_point proc near
-                        ; bx - byte address of cga pixel
-                        ; es - cga videobuffer address
+; bx - byte address of cga pixel
+; es - cga videobuffer address
     mov al, 1 ; color
     mov es:[bx], al
     ret
