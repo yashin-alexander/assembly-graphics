dd1e548a2c116a0d7d98b2f6be9363da310ba70b
yashin-alexander <alexandr.yashin@emlid.com>
2018-09-24 13:16:43 +0300

    update project structure
    - make.bat: add debug info to output file
    - update .gitignore

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 SET filename=MAIN
 
-TOOLS\TASM.EXE lab%1\SRC\%filename%.ASM /w2 /zi lab%1\BUILD\
-TOOLS\TLINK.EXE lab%1\BUILD\%filename%.OBJ, lab%1\BUILD\%1.EXE, lab%1\BUILD\%filename%.MAP
+TOOLS\TASM.EXE lab%1\SRC\%filename%.ASM /w2 /la /zi lab%1\BUILD\
+TOOLS\TLINK.EXE /v lab%1\BUILD\%filename%.OBJ, lab%1\BUILD\%1.EXE, lab%1\BUILD\%filename%.MAP
 COPY lab%1\BUILD\%1.exe %1.exe
